8251ed87f17f63db60f0b33d9aac137e20f42ead
simonireilly <simonireilly@gmail.com>
2022-05-30 21:59:18 +0100

    Setup the project

---
+++ src/builder/index.ts
@@ -0,0 +1,26 @@
+import { AppDataSource } from "../../examples/basic/src/data-source";
+import { DataSource, EntitySchema } from "typeorm";
+import { ConnectionMetadataBuilder } from "typeorm/connection/ConnectionMetadataBuilder";
+
+const entityMetaData = async (connection: DataSource) => {
+  const connectionMetadataBuilder = new ConnectionMetadataBuilder(connection);
+
+  const { entities } = connection.options;
+  const TEntities = entities as (Function | EntitySchema<any> | string)[];
+
+  let entityMetadata;
+
+  if (entities) {
+    entityMetadata = await connectionMetadataBuilder.buildEntityMetadatas(
+      TEntities
+    );
+  } else {
+    throw Error("No entities found on connection");
+  }
+
+  return entityMetadata;
+};
+
+export const builders = {
+  entityMetaData,
+};
