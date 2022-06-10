5890b6e5ae8c956cc2dcc08f1d66974caa99c0b9
simonireilly <simonireilly@gmail.com>
2022-06-10 23:29:20 +0100

    Add documentation to classes for the public methods on the adapter

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -9,6 +9,17 @@ const BUILDERS = {
 
 type TBuildTypes = keyof typeof BUILDERS;
 
+/**
+ * The ERD build returns the string representation of the ERD for the chosen
+ * markdown language. Provide an TypeORM DataSource to the initializer.
+ *
+ * @example
+ *
+ * const mermaid = new ERDBuilder("mermaid", AppDataSource);
+ * await mermaid.initialize();
+ * mermaid.render();
+ *
+ */
 class ERDBuilder {
   private instance: MermaidErd | PlantUMLErd;
 
@@ -17,10 +28,16 @@ class ERDBuilder {
     this.instance = new adapter(dataSource);
   }
 
+  /**
+   * Initialize the ERD to have the current entity relations for the DataSource
+   */
   public async initialize() {
     return this.instance.initialize();
   }
 
+  /**
+   * Render the ERD to a string
+   */
   public render(): string {
     return this.instance.render();
   }
